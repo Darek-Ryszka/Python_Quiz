a7897a65551e88b744a652a0ddd2baff4661e392
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-05-30 09:04:29 +0200

    Add display_score function

--- main.py
+++ main.py
@@ -29,8 +29,23 @@ def check_answer(answer, guess):
         return 0
 
 
-def display_score():
-    pass
+def display_score(correct_guesses, guesses):
+    print("-------------------------")
+    print("RESULTS")
+    print("-------------------------")
+
+    print("Answers: ", end="")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+
+    print("Guesses: ", end="")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score = int((correct_guesses / len(questions)) * 100)
+    print("Your score is: " + str(score) + "%")
 
 
 def play_again():
